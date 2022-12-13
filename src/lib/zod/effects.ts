52f8dddded0703abf2cc1d77da1a1b55ed2ce5f0
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2022-12-13 18:08:16 +0900

    chore: update zod version

--- src/lib/zod/effects.ts
+++ src/lib/zod/effects.ts
@@ -1,11 +1,8 @@
 import { z } from "zod";
 
 export const positiveInt = [
-  (v: string) => Number.isInteger(+v) && +v > 0,
+  (v: number) => Number.isInteger(v) && v > 0,
   "Require positive int.",
 ] as const;
 
-export const stringAsPositiveInt = z
-  .string()
-  .refine(...positiveInt)
-  .transform((v) => +v);
+export const asPositiveInt = z.coerce.number().refine(...positiveInt);
--- src/pages/my/posts/[postId].page.tsx
+++ src/pages/my/posts/[postId].page.tsx
@@ -2,7 +2,7 @@ import { GetServerSidePropsResult } from "next";
 import Link from "next/link";
 import { composeGssp, withZod, withLogin, z } from "@/lib/next";
 import { NotFoundGsspError } from "@/lib/next/error/gssp";
-import { stringAsPositiveInt } from "@/lib/zod/effects";
+import { asPositiveInt } from "@/lib/zod/effects";
 import { getMyPost } from "@/services/getMyPost";
 
 type Props = { title: string; body: string };
@@ -20,7 +20,7 @@ const Page = ({ title, body }: Props) => {
 
 export const getServerSideProps = composeGssp(
   withLogin,
-  withZod(z.object({ params: z.object({ postId: stringAsPositiveInt }) })),
+  withZod(z.object({ params: z.object({ postId: asPositiveInt }) })),
   async (ctx): Promise<GetServerSidePropsResult<Props>> => ({
     props: await getMyPost(ctx.params.postId).catch(() => {
       throw new NotFoundGsspError();
